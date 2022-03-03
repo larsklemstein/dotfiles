7f74f0236d829445c9176e0fb98f3a9fb0bf38a3
Lars Klemstein <lrsklemstein@gmail.com>
2022-03-03 14:58:24 +0100

    automated GoMono nerd font setting

--- dot_init.bash
+++ dot_init.bash
@@ -10,15 +10,14 @@ typeset GRUVBOX_GIT=https://github.com/morhetz/gruvbox
 
 typeset PROGNAME=${0##*/}
 
-
 msg() {
     echo "[$PROGNAME] $*" >&2
 }
 
-msg Hello
-
 for file in dot.*
 do
+    echo "File: $file"
+
     file_link=$HOME/.${file#dot.}
     file_dest="./$DIR_BASENAME/${file##*/}"
 
@@ -45,16 +44,20 @@ then
     msg "Created (empty) $common_interactive_local"
 fi
 
+
+# late tmp dir creation because for the beginning actions we need to
+# be inside the .dotfiles directory
+
+cd /tmp
+tmpdir=$(mktemp -d XXXXXXXXXX)
+trap "/bin/rm -rf $PWD/$tmpdir" 0 1 2
+
 vimdir=$HOME/.vim
 
 if [ ! -d "$vimdir" ]
 then
     msg "Installing Gruvbox vim colorscheme...."
     mkdir -p $vimdir
-
-    cd /tmp
-    tmpdir=$(mktemp -d XXXXXXXXXX)
-    trap "/bin/rm -rf $PWD/$tmpdir" 0 1 2
 
     cd $tmpdir
 
@@ -63,3 +66,28 @@ then
     mv gruvbox/colors $vimdir
     mv gruvbox/autoload $vimdir
 fi
+
+msg "Install GoMono font..."
+os_info=$(uname -v)
+
+case $os_info in
+    Darwin*)
+        font_dir=$HOME/Library/Fonts
+        ;;
+    *)
+        font_dir=$HOME/.local/share/fonts
+        ;;
+esac
+
+font_file=Go-Mono.zip
+
+font_url="https://github.com/ryanoasis/nerd-fonts/releases/download/"
+font_url+="v2.1.0/$font_file"
+
+wget $font_url
+
+test -d $font_dir || mkdir -p $_
+
+yes | unzip -d $font_dir $font_file
+
+msg "...Done! Copied font files to $font_dir"
