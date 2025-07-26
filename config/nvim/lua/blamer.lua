c73e60bcbb05ee852c135ebe2b66be10b5dd1b66
Lars Klemstein <lrsklemstein@gmail.com>
2025-07-26 12:14:48 +0200

    finetune nvim blamer config

--- config/nvim/lua/blamer.lua
+++ config/nvim/lua/blamer.lua
@@ -4,8 +4,13 @@ vim.g.blamer_delay = 750
 
 vim.g.blamer_show_in_visual_modes = 1
 vim.g.blamer_show_in_insert_modes = 1
-vim.g.blamer_prefix = ' > '
+
+vim.g.blamer_prefix = '   -- '
 
 -- Available options: <author>, <author-mail>, <author-time>, <committer>,
 -- <committer-mail>, <committer-time>, <summary>, <commit-short>, <commit-long>.
-vim.g.blamer_template = '<committer> =>  <summary>'
+vim.g.blamer_template = '<committer> <committer-time>: <summary>'
+
+vim.g.blamer_date_format = '%Y-%m-%d %H:%M'
+
+vim.g.blamer_relative_time = 0
