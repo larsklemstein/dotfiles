01729eba3f24ecdf6be2a57210a23e899974fcca
Lars Klemstein <lrsklemstein@gmail.com>
2025-07-27 13:15:56 +0200

    prepare lsp files; add installer for ls

---
+++ config/nvim/lsp/bin/install_lang_servers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -eu
+
+servers_from_npm=(
+    vscode-langservers-extracted
+    bash-language-server
+    dockerfile-language-server-nodejs
+    @microsoft/compose-language-service
+    typescript
+    typescript-language-server
+)
+
+servers_from_brew=(
+    pyright
+    rust-analyzer
+)
+
+for server in "${servers_from_npm[@]}"
+do
+    echo "Installing $server..." >&2
+    npm install -g "$server"
+done
+
+for server in "${servers_from_brew[@]}"
+do
+    echo "Installing $server..." >&2
+    brew install "$server"
+done
+
+echo "Done." >&2
