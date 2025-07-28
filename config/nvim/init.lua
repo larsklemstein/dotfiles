8ce8508da58f6f2bc86bbccb58e74d0895121ce4
Lars Klemstein <lrsklemstein@gmail.com>
2025-07-28 12:59:07 +0200

    fix nvim lint options

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,6 +10,11 @@ require("config.telescope")
 require("config.blamer")
 require("config.copilot")
 require("config.lsp")
-require("config.lint")
+-- Load lint config after plugins are loaded
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("config.lint")
+  end,
+})
 
 require("config.set_colorscheme")
